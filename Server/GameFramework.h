86cc82e8299e04bbbab12de9aad878646ce66c67
Goraney <btok92@gmail.com>
2021-11-10 20:27:00 +0900

    클라이언트로 송신하기 위한 struct ResponseMessage 구조체

--- Server/GameFramework.h
+++ Server/GameFramework.h
@@ -6,6 +6,28 @@
 //#include "Timer.h"
 //#include "Scene.h"
 
+#pragma pack(1)		// 구조체 멤버 맞춤은 1바이트 경계
+struct ResponseMessage
+{
+	XMFLOAT2 xmf2TankPosition[3];
+	XMFLOAT2 xmf2TankLook[3];
+	XMFLOAT2 xmf2TankVelocity[3];
+
+	int nPlayerHP[3];
+	int nPlayerScore[3];
+
+	bool bBulletsActive[30];
+	XMFLOAT2 xmf2BulletsPosition[30];
+	XMFLOAT2 xmf2BulletsLook[30];
+
+	bool bItemsActive[6];
+	XMFLOAT3 xmf3ItemsPosition[6];
+
+	int nCurrentRound;
+
+	bool bIsGameOver;
+};
+
 class CGameFramework
 {
 public:
@@ -35,5 +57,7 @@ private:
 	//CScene						*m_pScene = NULL;
 
 	_TCHAR						m_pszFrameRate[70];
+
+	struct ResponseMessage		m_message;
 };
 
