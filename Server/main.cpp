7ff679efead2365b2ca3314ec9d798b21fb2fb09
ParkJunYeong <junb0sday@gmail.com>
2021-11-10 17:40:06 +0900

    (서버) 미리 컴파일된 헤더 생성
    stdafx.h 파일 내에 필요 헤더, 라이브러리, 벡터관련 인라인 함수 작성

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,17 +1,4 @@
-#pragma comment(lib, "ws2_32")
-
-#include <iostream>
-#include <winsock2.h>
-
-#include <DirectXMath.h>
-#include <DirectXCollision.h>
-#include <DirectXPackedVector.h>
-
-using namespace std;
-
-using namespace DirectX;
-using namespace DirectX::PackedVector;
-
+#include "stdafx.h"
 
 int main() {
 	XMFLOAT3 xmf3Test = { 0.1f, 1.3f, 3.2f };
