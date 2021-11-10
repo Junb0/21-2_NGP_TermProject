3e42ef82e5a1fef708584b9fa1e683afc180438d
ParkJunYeong <junb0sday@gmail.com>
2021-11-10 17:10:56 +0900

    서버 프로젝트 DirectX관련 헤더 포함
    벡터 및 행렬연산, 충돌처리 객체 등 기능

--- Server/main.cpp
+++ Server/main.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 #include <winsock2.h>
 
+#include <DirectXMath.h>
+#include <DirectXCollision.h>
+#include <DirectXPackedVector.h>
+
 using namespace std;
 
-int main() {
+using namespace DirectX;
+using namespace DirectX::PackedVector;
 
+
+int main() {
+	XMFLOAT3 xmf3Test = { 0.1f, 1.3f, 3.2f };
+	cout << xmf3Test.x << " " << xmf3Test.y << " " << xmf3Test.z << endl;
 }
