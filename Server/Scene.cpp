77b1479a87b7a7084e2c3fa47bf1eec5b8e64294
ParkJunYeong <junb0sday@gmail.com>
2021-12-02 02:42:11 +0900

    (서버) 탱크 이동 완성 (클라 반응 확인)

--- Server/Scene.cpp
+++ Server/Scene.cpp
@@ -68,6 +68,7 @@ void CScene::ReleaseObjects() {
 void CScene::AnimateObjects(float fTimeElapsed) {
 	m_fElapsedTime = fTimeElapsed;
 	for (int i = 0; i < m_nGameObjects; ++i) m_ppGameObjects[i]->Animate(fTimeElapsed, NULL);
+	for (int i = 0; i < 3; ++i) m_ppTankObjects[i]->Animate(fTimeElapsed, NULL);
 }
 
 bool CScene::ProcessInput(DWORD dwDirection, bool bIsRotateLeft, bool bIsRotateRight, bool bIsFire, int nTankIndex, float fTimeElapsed)
