ed9cec3c73510579600d2b49b00f6189fc1ab6b0
Goraney <btok92@gmail.com>
2021-12-02 00:28:21 +0900

    (서버) 탱크 초기 데이터 입력

--- Server/Scene.cpp
+++ Server/Scene.cpp
@@ -10,6 +10,38 @@ CScene::~CScene() {
 }
 
 void CScene::BuildObjects() {
+	
+	//m_TankObjects[0].SetChild(pTankModelRed, true);
+	m_TankObjects[0].OnInitialize();
+	m_TankObjects[0].SetPosition(XMFLOAT3(13.0f, 0.0f, -5.0f));
+	m_TankObjects[0].SetScaleVar(1.0f, 1.0f, 1.0f);
+	m_TankObjects[0].Rotate(0.0f, 0.0f, 0.0f);
+	m_TankObjects[0].SetFriction(40.0f);
+	m_TankObjects[0].SetMaxVelocityXZ(12.0f);
+	m_TankObjects[0].SetAccelSpeedXZ(70.0f);
+	m_TankObjects[0].SetSpeedRotateY(180.0f);
+
+
+	//m_TankObjects[1].SetChild(pTankModelGreen, true);
+	m_TankObjects[1].OnInitialize();
+	m_TankObjects[1].SetPosition(XMFLOAT3(-3.0f, 0.0f, 30.0f));
+	m_TankObjects[1].SetScaleVar(1.0f, 1.0f, 1.0f);
+	m_TankObjects[1].Rotate(0.0f, 180.0f, 0.0f);
+	m_TankObjects[1].SetFriction(40.0f);
+	m_TankObjects[1].SetMaxVelocityXZ(12.0f);
+	m_TankObjects[1].SetAccelSpeedXZ(70.0f);
+	m_TankObjects[1].SetSpeedRotateY(180.0f);
+
+
+	//m_TankObjects[2].SetChild(pTankModelGreen, true);
+	m_TankObjects[2].OnInitialize();
+	m_TankObjects[2].SetPosition(XMFLOAT3(9.0f, 0.0f, 10.0f));	//위치 임의로 잡음
+	m_TankObjects[2].SetScaleVar(1.0f, 1.0f, 1.0f);
+	m_TankObjects[2].Rotate(0.0f, 90.0f, 0.0f);					//각도 임의로 잡음
+	m_TankObjects[2].SetFriction(40.0f);
+	m_TankObjects[2].SetMaxVelocityXZ(12.0f);
+	m_TankObjects[2].SetAccelSpeedXZ(70.0f);
+	m_TankObjects[2].SetSpeedRotateY(180.0f);
 
 }
 
